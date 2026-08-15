dc988ea5fe490f7468ee183be65e4f5419b29dce
NoCodeMO <goedsimpel@gmail.com>
2026-08-15 02:38:14 +0200

    Remove obsolete service worker

--- sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',e=>{self.skipWaiting()});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.registration.unregister()).then(()=>self.clients.matchAll()).then(cs=>Promise.all(cs.map(c=>c.navigate(c.url)))))});
